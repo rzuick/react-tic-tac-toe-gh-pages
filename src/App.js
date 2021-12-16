369a0a7dc4dc30b687f6159fc72389e8802b1218
Rebecca Z <80494343+rzuick@users.noreply.github.com>
2021-12-16 16:07:24 -0600

    modified check for winner method

--- src/App.js
+++ src/App.js
@@ -26,34 +26,29 @@ const generateSquares = () => {
 };
 
 const App = () => {
-  // This starts state off as a 2D array of JS objects with
-  // empty value and unique ids.
+
   const [squares, setSquares] = useState(generateSquares());
   const [player, changePlayer] = useState(PLAYER_1);
-  const [winner, checkWinner] = useState('');
+  // let [winner, checkWinner] = useState('');
+
   // Wave 2
-  // According to the README, App needs a method to update state of the board
-  // could be onClickCallback that is passed to Board
+
   const checkForWinner = () => {
-    // console.log('squares',squares);
     let row = 0;
-    let first = 0;
-    let middle = 1;
-    let last = 2;
     let column = 0;
-    // // check for row match
-    console.log('FIRST', squares[row][first].value);
-    while (row <= 3) {
-      if (squares[row][first].value === squares[row][middle].value 
-        === squares[row][last].value && squares[row][first].value != '') {
-        return squares[row][first].value;
+    // check for row match
+    while (row < 3) {
+      if (squares[row][0].value === squares[row][1].value 
+        === squares[row][2].value && squares[row][0].value != '') {
+        return squares[row][0].value;
       } 
-      else row +=1, first +=1, last +=1, middle +=1;
+      else row +=1;
     }
     // column check
-    while (column <= 3){
-      if (squares[0][column].value === squares[1][column].value 
-        === squares[2][column].value && squares[0][column] != '') {
+    while (column < 3){
+      if (squares[0][column].value === squares[1][column].value &&
+        squares[2][column].value === squares[1][column].value && 
+        squares[0][column].value != '') {
           return squares[0][column].value;
         }
         else column +=1;
@@ -65,9 +60,8 @@ const App = () => {
     }
     else if (squares[0][2].value === squares[1][1].value
       === squares[2][0].value && squares[0][2] != '') {
-        return squares[0][2];
-      }
-      return '';
+        return squares[0][2].value;
+      } return null;
   };
 
   const onClickCallback = (id) => {
@@ -86,8 +80,10 @@ const App = () => {
     else {
       changePlayer(PLAYER_1);
     }
-    checkWinner(checkForWinner());
+    
     setSquares(square);
+    // checkForWinner();
+    console.log('WINNER CHECK', checkForWinner());
     
   };
   // const resetGame = () => {
@@ -98,7 +94,7 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <h1>React Tic Tac Toe</h1>
-        <h2>The winner is ... {winner} </h2>
+        <h2>The winner is ... {} </h2>
         <button>Reset Game</button>
       </header>
       <main>
